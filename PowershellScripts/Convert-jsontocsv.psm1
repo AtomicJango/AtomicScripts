7a832dd22c460fdd60c65064205b11b58d602cd8
AtomicJango <gerardoggallegos@gmail.com>
2021-05-08 18:58:07 -0500

    git config --global "gerardoggallegos@gmail.com"

---
+++ PowershellScripts/Convert-jsontocsv.psm1
@@ -0,0 +1,15 @@
+
+
+funtion Convert-JsontoCSV{
+param (
+    [Parameter($jsonfilepath)]
+    [TypeName]
+    $ParameterName
+)
+
+Get-Content $jsonfilepath | ConvertFrom-Json | ConvertTo-Csv
+
+
+
+}
+Export-ModuleMember -Function Convert-JsontoCSV
